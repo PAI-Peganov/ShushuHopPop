2246c289e5dc72eedccfe38dbe7d445417cb8e55
Range18 <shakmate@mail.ru>
2025-05-05 11:33:29 +0500

    Update Menu

--- Assets/MainGameFolder/Scripts/UI/MainMenu/MainMenuManager.cs
+++ Assets/MainGameFolder/Scripts/UI/MainMenu/MainMenuManager.cs
@@ -1,12 +1,21 @@
-﻿using UnityEngine;
+﻿using System;
+using Unity.VisualScripting;
+using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.Serialization;
 
 namespace MainGameFolder.Scripts.UI.MainMenu
 {
     public class MainMenuManager : MonoBehaviour
     {
+        [FormerlySerializedAs("Menu")] [SerializeField]
+        private Canvas menu;
+
+        [FormerlySerializedAs("SettingsMenu")] [SerializeField]
+        private Canvas settingsMenu;
+
         // Starts the game from saved state
-        public void Play()  
+        public void Play()
         {
             //TODO: load game state
             SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
@@ -15,6 +24,14 @@ namespace MainGameFolder.Scripts.UI.MainMenu
         // Enables settings scene
         public void Settings()
         {
+            menu.enabled = false;
+            settingsMenu.enabled = true;
+        }
+
+        public void QuitSettings()
+        {
+            settingsMenu.enabled = false;
+            menu.enabled = true;
         }
 
         // Exists from game
