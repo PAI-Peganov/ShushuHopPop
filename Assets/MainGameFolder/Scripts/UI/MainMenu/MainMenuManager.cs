ea70ae2eb39cec85a398a81d31a6fb947bfa2385
Range18 <shakmate@mail.ru>
2025-05-05 10:26:50 +0500

    Create MainMenu Scene

---
+++ Assets/MainGameFolder/Scripts/UI/MainMenu/MainMenuManager.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace MainGameFolder.Scripts.UI.MainMenu
+{
+    public class MainMenuManager : MonoBehaviour
+    {
+        // Starts the game from saved state
+        public void Play()  
+        {
+            //TODO: load game state
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        }
+
+        // Enables settings scene
+        public void Settings()
+        {
+        }
+
+        // Exists from game
+        public void Exit()
+        {
+            Application.Quit();
+            Debug.Log("Exit");
+        }
+    }
+}
