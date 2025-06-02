df61a4c3322e6c3b51baa00e0a4cec5e923c6e1e
Range18 <shakmate@mail.ru>
2025-06-03 03:25:24 +0500

    Add hints

---
+++ Assets/MainGameFolder/Scripts/Hints/HintsManager.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+
+namespace MainGameFolder.Scripts.Hints
+{
+    public class HintsManager : MonoBehaviour
+    {
+        private Canvas hintUI;
+
+        void Start()    
+        {
+            hintUI = GetComponentInChildren<Canvas>();
+            hintUI.gameObject.SetActive(false);
+        }
+        
+        private void OnTriggerEnter2D(Collider2D other)
+        {
+            if (other.CompareTag("Player"))
+            {
+                Debug.Log("1111");
+                hintUI.gameObject.SetActive(true);
+            }
+        }
+
+        private void OnTriggerExit2D(Collider2D other)
+        {
+            if (other.CompareTag("Player"))
+            {
+                hintUI.gameObject.SetActive(false);
+            }
+        }
+    }
+}
