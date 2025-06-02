9e4cb755fa55209f1ac80982106d4332367c79a2
Артём Цыганов <tsiganovartem06@gmail.com>
2025-06-02 12:13:48 +0500

    menu done and fixed QTE

--- Assets/MainGameFolder/Scripts/UI/MainMenu/MainMenuManager.cs
+++ Assets/MainGameFolder/Scripts/UI/MainMenu/MainMenuManager.cs
@@ -1,4 +1,6 @@
-﻿using UnityEngine;
+﻿using Mono.Cecil;
+using UnityEngine;
+using UnityEngine.Rendering;
 using UnityEngine.SceneManagement;
 using UnityEngine.Serialization;
 
@@ -12,12 +14,34 @@ namespace MainGameFolder.Scripts.UI.MainMenu
         [FormerlySerializedAs("SettingsMenu")] [SerializeField]
         private Canvas settingsMenu;
 
+        [FormerlySerializedAs("History")]
+        [SerializeField]
+        private Canvas History;
+
+        private int history_label = 0;
+
         // Starts the game from saved state
         public void Play()
-        {
-            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        { 
+            history_label = 1;
         }
 
+        void Update()
+        {
+
+            History.enabled = (history_label == 1);
+            
+
+            if (history_label > 0 && Input.GetKeyDown(KeyCode.Mouse0))
+            {
+                history_label += 1;
+            }
+
+            if (history_label == 2)
+            {
+                SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+            }
+        }
         //TODO:
         // Enables settings scene
         // public void Settings()
